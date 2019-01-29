a7bd6f7c7b8bcfd1407e6e4215a2a3befbde3f8e
Jason Paredes <haloos@me.com>
2019-01-28 20:12:34 -0500

    Get & Populate Items

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ const ItemCtrl = (function(){
 
   // Public methods
   return {
+    getitems: function(){
+      return data.items;
+    },
     logData: function(){
       return data;
     }
@@ -34,7 +37,21 @@ const UICtrl = (function(){
 
   // Public methods
   return {
+    populateItemList: function(items){
+      let html = '';
 
+      items.forEach(function(item){
+          html += `<li class="collection-item" id="item-${item.id}">
+          <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+          <a href="#" class="secondary-content">
+            <i class="edit-item fa fa-pencil"></i>
+      </a>
+    </li>`;
+      });
+
+      // Insert list items
+      document.querySelector('#item-list').innerHTML = html;
+    }
   }
   
 })();
@@ -45,7 +62,11 @@ const App = (function(ItemCtrl, UICtrl){
   // Public methods
   return {
     init: function(){
-      console.log('App is online...');
+      // Fetch items from data structure
+      const items = ItemCtrl.getitems();
+
+      // Populate list with items
+      UICtrl.populateItemList(items);
     }
   }
   
